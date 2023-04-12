a35a50f30d9faa149acacfdd748aa3e85f3ad963
Shoxrux Husenov <husenovshoxrux71@gmail.com>
2023-04-13 04:53:33 +0500

    MainPage

---
+++ src/univer-management.Desktop/Accounts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace univer_management.Desktop
+{
+    public partial class Accounts : Form
+    {
+        public Accounts()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
