d48149b1d85376c986e24e81cbbeab8f86d82026
samandarbaxodirovich <samandar.lcl@gmail.com>
2023-04-16 01:01:23 +0500

    under conflick

--- src/univer-management.Desktop/UserControls2/UC_Auditoriya.cs
+++ src/univer-management.Desktop/UserControls2/UC_Auditoriya.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Guna.UI2.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using univer_management.Service.Services;
 
 namespace univer_management.Desktop.UserControls2
 {
     public partial class UC_Auditoriya : UserControl
     {
+        private readonly AuditoriyaService service = new();
+
         public UC_Auditoriya()
         {
             InitializeComponent();
+            Task.Run(async () =>
+            {
+                await SetValues();
+            });
         }
         private void DataGrid_Auditoriya(object sender, DataGridViewCellEventArgs e)
         {
@@ -24,6 +32,18 @@ namespace univer_management.Desktop.UserControls2
         private void guna2TextBox3_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void DataGridVIew_Auditor_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private async Task SetValues()
+        {
+            DataGridVIew_Auditor.Rows.Clear();
+            var targets = await service.GetAllAsync();
+            foreach (var item in targets)
+                DataGridVIew_Auditor.Rows.Add(item.Id, item.NumberOfOrder,item.AuditoriyaTipi,item.Capacity,item.Korpus);
         }
     }
 }
