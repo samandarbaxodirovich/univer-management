465a51f7fe365f5d0e5ed1f37ecf1413f7f0e165
samandarbaxodirovich <samandar.lcl@gmail.com>
2023-04-17 02:53:45 +0500

    OquvRejaViewModel

---
+++ src/univer-management.Service/ViewModels/OquvRejaViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace univer_management.Service.ViewModels
+{
+    public class OquvRejaViewModel
+    {
+        public int Id { get; set; }
+        public string FanName { get; set; } = string.Empty;
+        public double Lektsiya { get; set; }
+        public double Seminar { get; set; }
+        public double Labaratoriya { get; set; }
+        public double Zachot { get; set; }
+        public double DefZachot { get; set; }
+        public double Imtihon { get; set; }
+
+    }
+}
