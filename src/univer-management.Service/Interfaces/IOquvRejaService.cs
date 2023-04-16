e1ac7a926c724b5669563f8529b96ab7ba1eacf3
samandarbaxodirovich <samandar.lcl@gmail.com>
2023-04-17 02:17:39 +0500

    some

---
+++ src/univer-management.Service/Interfaces/IOquvRejaService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace univer_management.Service.Interfaces
+{
+    public interface IOquvRejaService
+    {
+        public Task<(bool, string)> CreateAsync();
+    }
+}
