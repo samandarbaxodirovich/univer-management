956df807015b1460bda9a9913ca42d9a82c7e4a9
Shoxrux Husenov <husenovshoxrux71@gmail.com>
2023-04-17 09:17:31 +0500

    bgfhjkhgfhjk

--- src/univer-management.DataAccess/Migrations/20230417041557_shjklssaa.Designer.cs
+++ src/univer-management.DataAccess/Migrations/20230417041557_shjklssaa.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using univer_management.DataAccess.DbContexts;
@@ -11,9 +12,10 @@ using univer_management.DataAccess.DbContexts;
 namespace univer_management.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230417041557_shjklssaa")]
+    partial class shjklssaa
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
