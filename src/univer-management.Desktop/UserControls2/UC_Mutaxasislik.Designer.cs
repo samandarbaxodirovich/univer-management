7de6f15e1da7468eb35acef2d59a8085a21db42d
zokirjanov <zokirjonovshohruxbek1016@gmail.com>
2023-04-28 21:55:25 +0500

    aa

--- src/univer-management.Desktop/UserControls2/UC_Mutaxasislik.Designer.cs
+++ src/univer-management.Desktop/UserControls2/UC_Mutaxasislik.Designer.cs
@@ -1,4 +1,4 @@
-namespace univer_management.Desktop.UserControls2
+﻿namespace univer_management.Desktop.UserControls2
 {
     partial class UC_Mutaxasislik
     {
@@ -40,6 +40,11 @@ namespace univer_management.Desktop.UserControls2
 			this.checkBox1 = new System.Windows.Forms.CheckBox();
 			this.textBox3 = new System.Windows.Forms.TextBox();
 			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Mutaxassisliknomi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Update = new System.Windows.Forms.DataGridViewButtonColumn();
+			this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
 			this.panel1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
@@ -103,10 +108,18 @@ namespace univer_management.Desktop.UserControls2
 			// 
 			// dataGridView1
 			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
 			this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.Mutaxassisliknomi,
+            this.Update,
+            this.Delete});
 			this.dataGridView1.Location = new System.Drawing.Point(31, 184);
 			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
 			this.dataGridView1.RowHeadersVisible = false;
 			this.dataGridView1.RowHeadersWidth = 51;
 			this.dataGridView1.RowTemplate.Height = 29;
@@ -166,6 +179,47 @@ namespace univer_management.Desktop.UserControls2
 			this.textBox2.Size = new System.Drawing.Size(283, 30);
 			this.textBox2.TabIndex = 63;
 			// 
+			// ID
+			// 
+			this.ID.HeaderText = "ID";
+			this.ID.MinimumWidth = 6;
+			this.ID.Name = "ID";
+			this.ID.Width = 125;
+			// 
+			// dataGridViewTextBoxColumn1
+			// 
+			this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+			this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+			this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+			this.dataGridViewTextBoxColumn1.ReadOnly = true;
+			this.dataGridViewTextBoxColumn1.Width = 50;
+			// 
+			// Mutaxassisliknomi
+			// 
+			this.Mutaxassisliknomi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Mutaxassisliknomi.HeaderText = "Mutaxassislik nomiv";
+			this.Mutaxassisliknomi.MinimumWidth = 6;
+			this.Mutaxassisliknomi.Name = "Mutaxassisliknomi";
+			this.Mutaxassisliknomi.ReadOnly = true;
+			// 
+			// Update
+			// 
+			this.Update.HeaderText = "Update";
+			this.Update.MinimumWidth = 6;
+			this.Update.Name = "Update";
+			this.Update.ReadOnly = true;
+			this.Update.Text = "✏️";
+			this.Update.UseColumnTextForButtonValue = true;
+			// 
+			// Delete
+			// 
+			this.Delete.HeaderText = "Delete";
+			this.Delete.MinimumWidth = 6;
+			this.Delete.Name = "Delete";
+			this.Delete.ReadOnly = true;
+			this.Delete.Text = "X";
+			this.Delete.UseColumnTextForButtonValue = true;
+			// 
 			// UC_Mutaxasislik
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -205,5 +259,10 @@ namespace univer_management.Desktop.UserControls2
 		private CheckBox checkBox1;
 		private TextBox textBox3;
 		private TextBox textBox2;
+		private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+		private DataGridViewTextBoxColumn Mutaxassisliknomi;
+		private DataGridViewButtonColumn Update;
+		private DataGridViewButtonColumn Delete;
+		private DataGridViewTextBoxColumn ID;
 	}
 }
